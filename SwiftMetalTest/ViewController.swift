77150b5cdbd55f3672437134c5ca91af8b044a34
t79 <terje@urnes.net>
2016-09-22 01:10:02 +0200

    Refactoring View Controller after moving parts out to other classes

--- SwiftMetalTest/ViewController.swift
+++ SwiftMetalTest/ViewController.swift
@@ -14,16 +14,11 @@ class ViewController: UIViewController {
     
     var device: MTLDevice! = nil
     var metalLayer: CAMetalLayer! = nil
-    var vertexBuffer: MTLBuffer! = nil
     var pipelineState: MTLRenderPipelineState! = nil
     var commandQueue: MTLCommandQueue! = nil
     var timer: CADisplayLink! = nil
     
-    let vertexData: [Float] = [
-             0.0,  1.0, 0.0,
-            -1.0, -1.0, 0.0,
-             1.0, -1.0, 0.0
-    ]
+    var objectToDraw: Triangle!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,8 +32,7 @@ class ViewController: UIViewController {
         metalLayer.frame = view.layer.frame
         view.layer.addSublayer(metalLayer)
         
-        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
-        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize)
+        objectToDraw = Triangle(device: device)
         
         let defaultLibrary = device.newDefaultLibrary()
         let fragmentProgram = defaultLibrary!.makeFunction(name: "basic_fragment")
@@ -68,23 +62,7 @@ class ViewController: UIViewController {
     
     func render() {
         let drawable = metalLayer.nextDrawable()!
-        
-        let renderPassDescriptor = MTLRenderPassDescriptor()
-        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
-        renderPassDescriptor.colorAttachments[0].loadAction = .clear
-        renderPassDescriptor.colorAttachments[0].clearColor =
-                MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
-        
-        let commandBuffer = commandQueue.makeCommandBuffer()
-        
-        let renderEncoderOpt = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        renderEncoderOpt.setRenderPipelineState(pipelineState)
-        renderEncoderOpt.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
-        renderEncoderOpt.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
-        renderEncoderOpt.endEncoding()
-        
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
+        objectToDraw.render(commandQueue: commandQueue, pipelineState: pipelineState, drawable: drawable, clearColor: nil)
     }
     
     func gameloop() {
