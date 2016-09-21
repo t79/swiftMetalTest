9684fa98ffd822a9aae4358dd4908fb923553be3
t79 <terje@urnes.net>
2016-09-21 23:35:52 +0200

    Step 2. Creating a CAMetalLayer and added to view's main layer

--- SwiftMetalTest/ViewController.swift
+++ SwiftMetalTest/ViewController.swift
@@ -8,15 +8,24 @@
 
 import UIKit
 import Metal
+import QuartzCore
 
 class ViewController: UIViewController {
     
     var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         device = MTLCreateSystemDefaultDevice()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
     }
 
     override func didReceiveMemoryWarning() {
