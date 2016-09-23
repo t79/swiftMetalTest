117107270ffca5c73d989ebf3708e7b1ba82790d
t79 <terje@urnes.net>
2016-09-24 01:39:04 +0200

    Fixing resolution on retina displays. and fix perspective when going from portage to landscape.

--- SwiftMetalTest/MetalViewController.swift
+++ SwiftMetalTest/MetalViewController.swift
@@ -38,7 +38,6 @@ class MetalViewController: UIViewController {
         metalLayer.device = device
         metalLayer.pixelFormat = .bgra8Unorm
         metalLayer.framebufferOnly = true
-        metalLayer.frame = view.layer.frame
         view.layer.addSublayer(metalLayer)
         
         let defaultLibrary = device.newDefaultLibrary()
@@ -67,6 +66,20 @@ class MetalViewController: UIViewController {
         
         timer = CADisplayLink(target: self, selector: #selector(MetalViewController.newFrame))
         timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let window = view.window {
+            let scale = window.screen.nativeScale
+            let layerSize = view.bounds.size
+            view.contentScaleFactor = scale
+            metalLayer.frame = CGRect(x: 0, y: 0, width: layerSize.width, height: layerSize.height)
+            metalLayer.drawableSize = CGSize(width: layerSize.width * scale, height: layerSize.height * scale)
+        }
+        
+        projectionMatrix = float4x4.makePerspectiveViewAngle(float4x4.degrees(toRad: 85), aspectRatio: Float(self.view.bounds.size.width / self.view.bounds.size.height), nearZ: 0.01, farZ: 100.0)
     }
 
     override func didReceiveMemoryWarning() {
