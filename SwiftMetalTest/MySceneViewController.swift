d9c19d928863bb785465fb1b6ebaf7c5dc9f8a69
t79 <terje@urnes.net>
2016-09-23 21:25:29 +0200

    Adding MySceneViewController class

---
+++ SwiftMetalTest/MySceneViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MySceneViewController.swift
+//  SwiftMetalTest
+//
+//  Created by Terje Urnes on 23.09.2016.
+//  Followed tutorial by Ray Wenderlich, see README.md
+//
+
+import UIKit
+
+class MySceneViewController: MetalViewController, MetalViewControllerDelegate {
+    
+    func renderObjects(drawable: CAMetalDrawable) {
+        // TODO
+    }
+    
+    func updateLogic(timeSinceLastUpdate: CFTimeInterval) {
+        // TODO
+    }
+    
+}
